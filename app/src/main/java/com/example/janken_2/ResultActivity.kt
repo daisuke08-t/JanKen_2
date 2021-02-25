a00e1e12b23cf2e9471f5c675dbb97dbcd6b2c08
dai03 <d.taniguchi0803@gmail.com>
2021-02-26 00:59:11 +0900

    work_20210225:Intent ResultActivity

---
+++ app/src/main/java/com/example/janken_2/ResultActivity.kt
@@ -0,0 +1,23 @@
+package com.example.janken_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.janken_2.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val id = intent.getIntExtra("MY_HAND", 0)
+
+        when(id){
+            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
+            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
+            R.id.pa -> binding.myHandImage.setImageResource(R.drawable.pa)
+        }
+    }
+
+
+}
