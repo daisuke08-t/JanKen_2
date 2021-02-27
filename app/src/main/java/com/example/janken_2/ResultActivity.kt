92d2c5615ab61fdd2d34b6aa7130c91aa122c1cc
dai03 <d.taniguchi0803@gmail.com>
2021-02-27 10:28:34 +0900

    work_20210225:Modify ResultActivity.kt

--- app/src/main/java/com/example/janken_2/ResultActivity.kt
+++ app/src/main/java/com/example/janken_2/ResultActivity.kt
@@ -5,17 +5,54 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.janken_2.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    //じゃんけんの手の定数
+    val gu = 0
+    val choki = 1
+    val pa = 2
+    //onCreate内で初期化する
     private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //MainActivityより選択した手の画像を取得
         val id = intent.getIntExtra("MY_HAND", 0)
 
-        when(id){
-            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
-            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
-            R.id.pa -> binding.myHandImage.setImageResource(R.drawable.pa)
+        val myHand: Int
+
+        //idに格納されている画像に応じて、myHandImageにセット＆手の定数をリターン
+        myHand = when(id){
+            R.id.gu -> {
+                binding.myHandImage.setImageResource(R.drawable.gu)
+                gu
+            }
+            R.id.choki -> {
+                binding.myHandImage.setImageResource(R.drawable.choki)
+                choki
+            }
+            R.id.pa -> {
+                binding.myHandImage.setImageResource(R.drawable.pa)
+                pa
+            }
+            else -> {
+                gu
+            }
+        }
+
+        //コンピュータの手を決める
+        val comHand = (Math.random() * 3).toInt()
+        when(comHand) {
+            gu -> binding.comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> binding.comHandImage.setImageResource(R.drawable.com_choki)
+            pa -> binding.comHandImage.setImageResource(R.drawable.com_pa)
+        }
+
+        //勝敗の判定
+        val gameResult = (comHand - myHand + 3) % 3
+        when(gameResult){
+            0 -> binding.resultLabel.setText(R.string.result_draw)
+            1 -> binding.resultLabel.setText(R.string.result_win)
+            2 -> binding.resultLabel.setText(R.string.result_lose)
         }
     }
 
