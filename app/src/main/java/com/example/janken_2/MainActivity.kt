1f10f18c91f56698a6a6df5cafd08a1265e9026d
dai03 <d.taniguchi0803@gmail.com>
2021-02-28 20:19:45 +0900

    work_20210225:ResultActivity add saveData method

--- app/src/main/java/com/example/janken_2/MainActivity.kt
+++ app/src/main/java/com/example/janken_2/MainActivity.kt
@@ -7,17 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.janken_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //gu,choki,paの画像をタップしたときに、画面遷移する
         binding.gu.setOnClickListener { onJankenButtonTapped(it) }
         binding.choki.setOnClickListener { onJankenButtonTapped(it) }
         binding.pa.setOnClickListener { onJankenButtonTapped(it) }
     }
-
+    //ResultActivityに画面遷移するメソッド
     fun onJankenButtonTapped(view: View?){
         val intent = Intent(this, ResultActivity::class.java)
         intent.putExtra("MY_HAND", view?.id)
--- app/src/main/java/com/example/janken_2/ResultActivity.kt
+++ app/src/main/java/com/example/janken_2/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.janken_2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.example.janken_2.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -54,6 +55,33 @@ class ResultActivity : AppCompatActivity() {
             1 -> binding.resultLabel.setText(R.string.result_win)
             2 -> binding.resultLabel.setText(R.string.result_lose)
         }
+        //MainActivityに戻る処理
+        binding.backButton.setOnClickListener { finish() }
+    }
+
+    //じゃんけんの結果を保存する
+    private fun saveData(myHand: Int, comHand: Int, gameResult: Int){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+        val lastComHand = pref.getInt("LAST_COM_HAND", 0)
+        val lastGameResult = pref.getInt("GAME_RESULT", -1)
+
+        val edtWinningStreakCount: Int =
+                when{
+                    lastGameResult == 2 && gameResult == 2 ->
+                        winningStreakCount + 1
+                    else -> 0
+                }
+
+        val editor = pref.edit()
+        editor.putInt("GAME_COUNT", gameCount + 1)
+                .putInt("WINNING_STREAK_COUNT", edtWinningStreakCount)
+                .putInt("LAST_COM_HAND", comHand)
+                .putInt("LAST_MY_HAND", myHand)
+                .putInt("BEFORE_LAST_COM_HAND", lastComHand)
+                .putInt("GAME_RESULT", gameResult)
+                .apply()
     }
 
 
